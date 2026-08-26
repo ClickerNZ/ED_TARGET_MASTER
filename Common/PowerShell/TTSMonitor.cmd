c2962cc5663ee705414ce1648beb13dab535a313
Clicker <click@monds.co.nz>
2026-08-26 22:04:42 +1200

    Update TTSMonitor.cmd

--- Common/PowerShell/TTSMonitor.cmd
+++ Common/PowerShell/TTSMonitor.cmd
@@ -2,7 +2,8 @@
 cls
 echo.
 
-start powershell.exe -ExecutionPolicy Bypass -File "C:\Thrustmaster\Common\PowerShell\TTSMonitor.ps1"
+rem start powershell.exe -ExecutionPolicy Bypass -File "C:\Thrustmaster\Common\PowerShell\TTSMonitor.ps1"
+start powershell.exe -ExecutionPolicy Bypass -File "C:\Thrustmaster\Common\PowerShell\TTSMonitor-v29a.ps1"
 
 rem pause to catch startup errors
 pause
